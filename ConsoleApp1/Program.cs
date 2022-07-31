07059f19c15ea52fbb9e85d68915f749df3fd1c7
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-07-31 06:05:46 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems" + "\n" + "Remember, we're \"accounting\" for you");
+        Console.ReadLine();
+    }
+}
+
